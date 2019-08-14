6af68e75941178205a12113e4386047e809216f5
tonokim <tonokim@qq.com>
2019-08-14 11:40:05 +0800

    add file-loader

--- next.config.js
+++ next.config.js
@@ -13,6 +13,31 @@ module.exports = withLess({
     localIdentName: process.env.NODE_ENV === 'production'? "[hash:base64:5]" : "[name]__[local]___[hash:base64:5]",
   },
   webpack: function (config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': `${__dirname}`,
+    }
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name(file) {
+                  if (process.env.NODE_ENV === 'production') {
+                    return '[contenthash].[ext]';
+                  }
+                  return '[path][name].[ext]';
+                },
+                publicPath: "/_next/static/images",
+                outputPath: "static/images/",
+              },
+            }
+        ]
+      }
+    ]
     return config;
   }
 });
